d366364fae3af4b0acd0413ef6aaa50e1d2ddfae
Seda Malkoç <sedaamalkocc@gmail.com>
2024-01-27 19:50:12 +0300

    Courses

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "bulma/css/bulma.css";
+import "./App.css";
+import Course from "./Components/Course";
+import Angular from "./images/angular.png";
+import Bootstrap from "./images/bootstrap.jpg";
+import ReactJs from "./images/reactjs.png";
+import ReactNative from "./images/reactnative.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="hero is-link">
+        <div className="hero-body">
+          <p className="title">Courses</p>
+        </div>
+      </section>
+      <div className="container">
+        <section className="section">
+          <div className="columns">
+            <div className="column">
+              <Course
+                image={Angular}
+                title="Angular"
+                description="Angular, HTML, CSS ve JavaScript/TypeScript'i bir araya getirerek dinamik ve etkileşimli web uygulamaları oluşturmayı sağlar. İki yönlü veri bağlama özelliği, bileşen tabanlı mimarisi, routing, formlar yönetimi gibi birçok özelliğiyle geliştiricilere kolaylık sağlar. Angular, kapsamlı bir dokümantasyona ve geniş bir geliştirici topluluğuna sahiptir. Bu özellikleriyle, karmaşık ve büyük ölçekli web uygulamaları geliştirmek için tercih edilen bir çatıdır."
+              />
+            </div>
+            <div className="column">
+              <Course
+                image={Bootstrap}
+                title="Bootstrap"
+                description="Bootstrap, web geliştiricilerin kullanımına sunulan bir açık kaynaklı CSS ve JavaScript kütüphanesidir. İlk olarak Twitter tarafından geliştirilmiştir ve web uygulamalarını hızlı bir şekilde oluşturmak için tasarlanmıştır. Bootstrap, kullanıcı arayüzü tasarımında yaygın olarak kullanılan bir dizi hazır bileşen ve stiller sunar. Bunlar arasında düğmeler, form alanları, navigasyon menüleri, tipografi ve daha fazlası bulunur. "
+              />
+            </div>
+            <div className="column">
+              <Course
+                image={ReactJs}
+                title="React"
+                description="React, sanal DOM (Document Object Model) kullanarak performansı artıran bir yapıya sahiptir. Bu sayede, uygulama içerisindeki değişikliklerin DOM'a yansıtılması daha verimli bir şekilde gerçekleşir. React ayrıca, bileşenler arasında veri akışını yönetmek için tek yönlü veri akışı prensibini kullanır. Bu da uygulamanın daha öngörülebilir ve hata ayıklamasının daha kolay olmasını sağlar."
+              />
+            </div>
+            <div className="column">
+              <Course
+                image={ReactNative}
+                title="React Native"
+                description="React Native, Facebook tarafından geliştirilen bir mobil uygulama geliştirme çerçevesidir. JavaScript ve React.js kütüphanesini kullanarak iOS ve Android platformları için native (doğal) mobil uygulamalar oluşturmanıza olanak tanır. React Native, aynı kod tabanını paylaşarak, farklı platformlarda hızlı bir şekilde uygulama geliştirmeyi sağlar. Bu, geliştiricilerin iş akışını hızlandırır ve maliyeti düşürür. React Native, performansı ve kullanıcı deneyimini artırmak için platforma özgü bileşenleri ve API'leri kullanır. "
+              />
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
---
+++ src/Components/Course.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Course({ title, description, image }) {
+  return (
+    <div className="section">
+      <div className="card">
+        <div className="card-image">
+          <figure className="image is-4by3">
+            <img src={image} alt="Images" />
+          </figure>
+        </div>
+        <div className="card-content">
+          <div className="media">
+            <div className="media-content">
+              <p className="title is-4">{title}</p>
+            </div>
+          </div>
+
+          <div className="content">{description}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Course;
